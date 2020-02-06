17abab954c015a6e6c0b305826f7c186296eba22
marezi <markoilic22122012@gmail.com>
2020-02-06 20:04:47 +0100

    Admin Professor Can See All Subjects

--- app/src/main/java/martin/sms/Admin/AdminSubjectListAdapter.java
+++ app/src/main/java/martin/sms/Admin/AdminSubjectListAdapter.java
@@ -1,18 +1,21 @@
 package martin.sms.Admin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
 import martin.sms.R;
 import martin.sms.Subject;
+
 
 public class AdminSubjectListAdapter extends ArrayAdapter<Subject> {
 
@@ -35,15 +38,39 @@ public class AdminSubjectListAdapter extends ArrayAdapter<Subject> {
         TextView subjectGodina = (TextView)listView.findViewById(R.id.godina);
         TextView subjectSmer = (TextView)listView.findViewById(R.id.smer);
         TextView subjectPredmet = (TextView)listView.findViewById(R.id.predmet);
+        final TextView subjectId = (TextView)listView.findViewById(R.id.idPredmet);
 
 
         Subject subject = subjectList.get(position);
         subjectGodina.setText(subject.getGodina());
         subjectSmer.setText(subject.getSmer());
         subjectPredmet.setText(subject.getNaziv());
+        subjectId.setText(subject.getSubjectID());
 
+        Button professor = (Button) listView.findViewById(R.id.profesori);
+        professor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AllProfessorsBySubject.class);
+                intent.putExtra("subjectID", subjectId.getText());
+                view.getContext().startActivity(intent);
+            }
+        });
 
         return listView;
     }
 }
+
+/*
+* Button professor = (Button) findViewById(R.id.profesori);
+        professor.setOnClickListener((view) -> {
+
+            Intent intent = new Intent(this, Page.class);
+            intent.putExtra("subjectID", subjectID); // getText() SHOULD NOT be static!!!
+            startActivity(intent);
+        });
+
+String passedArg = getIntent().getExtras().getString("arg");
+enteredValue.setText(passedArg); Ovo je za drugu aktivnost
+*  */
 
--- app/src/main/java/martin/sms/Admin/AllProfessorsBySubject.java
+++ app/src/main/java/martin/sms/Admin/AllProfessorsBySubject.java
@@ -1,15 +1,13 @@
 package martin.sms.Admin;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,31 +18,29 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import martin.sms.LoginActivity;
-import martin.sms.Professor.ProfessorEvidenceList;
-import martin.sms.Professor.ProfessorEvidenceListAdapter;
-import martin.sms.Professor.ProfileActivityProfessor;
+import martin.sms.Professor.Profesor;
 import martin.sms.R;
-import martin.sms.Student.MainActivity;
-import martin.sms.Student.Student;
 import martin.sms.Subject;
 
-public class AdminAllSubjectList extends AppCompatActivity {
+public class AllProfessorsBySubject extends AppCompatActivity {
 
     private ListView listView;
-    List<Subject> subjectList;
+    List<Profesor> professorList;
     private FirebaseAuth auth;
     private FirebaseDatabase firebaseDatabase;
-   // private FirebaseAuth.AuthStateListener authListener;
+    // private FirebaseAuth.AuthStateListener authListener;
     private DatabaseReference myRef;
     String subjectID;
     //Button ok;
+
+     //String subjectID = getIntent().getExtras().getString("subjectID");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_all_subject_list);
+        setContentView(R.layout.activity_admin_all_professors_by_subject_list);
+        //Toast.makeText(this, "SubjectID je: " + subjectID, Toast.LENGTH_LONG).show();
 
         auth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -52,13 +48,14 @@ public class AdminAllSubjectList extends AppCompatActivity {
 
         //Button ok = (Button) findViewById(R.id.btnOk);
         listView = findViewById(R.id.listview);
-        subjectList = new ArrayList<>();
+        professorList = new ArrayList<>();
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
         if(bundle != null)
             subjectID = bundle.getString("subjectID");
+
 
     }
 
@@ -69,21 +66,23 @@ public class AdminAllSubjectList extends AppCompatActivity {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         Query query = reference
                 .child("subjects")
-                .orderByChild("smer");
+                .child(subjectID)
+                .child("professors");
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for ( DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
-                    Subject subject = singleSnapshot.getValue(Subject.class);
-                    subjectList.add(subject);
+                    Profesor professor = singleSnapshot.getValue(Profesor.class);
+                    professorList.add(professor);
                 }
 
                 Query query = FirebaseDatabase.getInstance().getReference()
-                        .child("subjects");
+                        .child("subjects")
+                        .child(subjectID);
                 query.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        AdminSubjectListAdapter adapter = new AdminSubjectListAdapter(AdminAllSubjectList.this, subjectList);
+                        ProfessorsBySubjectListAdapter adapter = new ProfessorsBySubjectListAdapter(AllProfessorsBySubject.this, professorList);
                         listView.setAdapter(adapter);
                     }
 
